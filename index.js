0a60b25c4707c9e26c02592e69e638257a83b147
github username <josephtrettevik@gmail.com>
2019-09-19 08:52:09 -0700

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,30 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let greeting = '';
+  let timeArray = timeString.split('');
+  let copyTimeArray = []
+  for(let i = 0; i < timeArray.length; i++) {
+    if (timeArray[i] !== ':') {
+      copyTimeArray.push(timeArray[i]);
+    }
+  }
+  timeString = copyTimeArray.join('');
+  const time = parseInt(timeString);
+  if (0 <= time && time < 1200 ) {
+    greeting = "Good Morning"
+  } else if (1200 <= time && time <= 1700) {
+    greeting = "Good Afternoon"
+  } else if (1700 < time && time <= 2359) {
+    greeting = "Good Evening"
+  } else {
+    greeting = "Please enter a valid time"
+  }
+  return greeting;
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(greeting){
+  const element = document.getElementById("greeting");
+  element.innerText= greeting;
+}
